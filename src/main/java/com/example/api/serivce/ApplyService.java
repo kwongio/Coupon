d2842ce0620e4c816451164f000d75a17afb314d
kwongio <rldh11111@naver.com>
2023-10-22 18:45:35 +0900

    선착순 쿠폰 redis로 해결

---
+++ src/main/java/com/example/api/serivce/ApplyService.java
@@ -0,0 +1,23 @@
+package com.example.api.serivce;
+
+import com.example.api.domain.Coupon;
+import com.example.api.repository.CouponCountRepository;
+import com.example.api.repository.CouponRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ApplyService {
+    private final CouponRepository couponRepository;
+    private final CouponCountRepository couponCountRepository;
+
+    public void apply(Long userId) {
+        Long count = couponCountRepository.increment();
+        if (count > 100) {
+            return;
+        }
+        couponRepository.save(new Coupon(userId));
+    }
+}
---
+++ src/test/java/com/example/api/ApiApplicationTests.java
@@ -0,0 +1,56 @@
+package com.example.api;
+
+import com.example.api.repository.CouponRepository;
+import com.example.api.serivce.ApplyService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Autowired
+    private ApplyService applyService;
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Test
+    void 한번만응모() {
+        applyService.apply(1L);
+        long count = couponRepository.count();
+
+        assertThat(count).isEqualTo(1);
+    }
+
+    @Test
+    public void 여려명응모() throws InterruptedException {
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(userId);
+
+                } finally {
+                    latch.countDown();
+                }
+            });
+
+        }
+        latch.await();
+
+        long count = couponRepository.count();
+        assertThat(count).isEqualTo(100);
+        System.out.println("count = " + count);
+    }
+
+}
